f3256f24b91532fd2210a286777997c63851eb4e
EvanGertis <physevangertis@gmail.com>
2022-01-25 20:19:34 -0500

    emergency commit

--- Module 9/ParseXML/src/main/Main.java
+++ Module 9/ParseXML/src/main/Main.java
@@ -11,6 +11,9 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class Main {
@@ -65,12 +68,29 @@ public class Main {
 	}
 	
 	public static void proccessKeyTermsAndDescriptions(Document doc) {
-		System.out.println(doc.getDocumentElement().getNodeName());
 		
-		//TODO: add keyterms to array.
-		
-		
-		//TODO: add descriptions to array.
+		NodeList list = doc.getElementsByTagName("exercise");
+		for(int temp = 0; temp < list.getLength(); temp++) {
+			
+			Node node = list.item(temp);
+			
+			if(node.getNodeType() == Node.ELEMENT_NODE) {
+				
+				// Identify the root element.			
+				Element element = (Element) node;
+				String key = element.getElementsByTagName("key").item(0).getTextContent();				
+//				String keyterm = element.getElementsByTagName("keyterm").item(0).getTextContent();
+//				String description = element.getElementsByTagName("description").item(0).getTextContent();
+//				System.out.println(keyterm);
+//				System.out.println(description);
+				//TODO: add keyterms to array.
+//				System.out.println(key);
+				
+				//TODO: add descriptions to array.
+//				System.out.println(description);
+			}
+			
+		}
 		
 	}
 	
